a17bb621fb2400b3cae0d788bb83fbedd875851b
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-08-28 17:37:51 -0500

    update-projects

--- src/pages/Front.js
+++ src/pages/Front.js
@@ -6,31 +6,46 @@ const Front = () => {
     <div className='projects' >
       Front Projects
       <div className=" project"  >
-      <h3>projectName</h3>
-       <Link to='/'>app</Link>
-       <Link to='/'>Github</Link> 
+      <h3>Beers Store</h3>
+      <div>
+      <a href='https://barbaro-store-beers.netlify.app/'>App</a>
+       <a href='https://github.com/barbaroreyes/app-store-beers'>Github</a> 
+      </div>
         </div>
         <div className=" project"  >
-        <h3>projectName</h3>
-       <Link to='/'>app</Link>
-       <Link to='/'>Github</Link> 
+        <h3>Search Friends</h3>
+        <div>
+        <a href='https://elastic-rosalind-30a9ac.netlify.app/' to='/'>App</a>
+       <a href='https://github.com/barbaroreyes/newT2/tree/master/src'>Github</a> 
+        </div>
+       
         </div>
 
 <div className=" project"  >
-<h3>projectName</h3>
-       <Link to='/'>app</Link>
-       <Link to='/'>Github</Link> 
+<h3>Store Pets</h3>
+<div>
+<a href='https://add-your-pet.netlify.app/'>App</a>
+       <a href='https://github.com/barbaroreyes/dogs-app-frontend'>Github</a> 
+</div>
+    
         </div>
         <div className=" project"  >
-        <h3>projectName</h3>
-       <Link to='/'>app</Link>
-       <Link to='/'>Github</Link> 
-        app github
+        <h3>Ecommerce</h3>
+        <div>
+        <a href='https://hungry-einstein-27344a.netlify.app/'>App</a>
+       <a href='https://github.com/barbaroreyes/EcomerceCLone'>Github</a> 
+        
+        </div>
+    
         </div>
         <div className=" project"  >
-        <h3>projectName</h3>
-       <Link to='/'>app</Link>
-       <Link to='/'>Github</Link> 
+        <h3>Seven Wonder</h3>
+        <div>
+        <a href='https://mystifying-panini-451434.netlify.app/'>App</a>
+       <a href='https://github.com/barbaroreyes/seven_wonders-frontend'>Github</a> 
+        </div>
+       
+       <div></div>
         </div> 
     </div>
     
