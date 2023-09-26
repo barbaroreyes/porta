550b270e38c9e8afa7105f4c49c16643cad8b869
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-09-26 16:21:37 -0500

    update

--- src/compo/Nav.js
+++ src/compo/Nav.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import {useState}from 'react';
 import {Link} from 'react-router-dom'
+import Hamburger from 'hamburger-react'
+
 const Nav = () => {
+  const[open , setOpen] = useState(false)
+
+
+
   return (
     <div className="nav_container">
-      <div></div>
-      <div></div>
-      <div></div>
+    <Hamburger color = 'white' size={30} duration={1} onToggle={toggle =>{
+      if(toggle){
+        console.log('open Menu')
+      }else{
+        console.log('close Menu')
+      }
+    }}/>
+      
       <nav className="regular_nav">
         <Link to='/'>Home</Link>
         <Link to='/projects'>Projects</Link>
